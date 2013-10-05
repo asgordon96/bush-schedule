7012060f8e783c67616d3eb4fc7e9591fb874c03
Aaron Gordon <asgordon96@gmail.com>
2013-10-05 11:14:35 -0700

    Add the most basic app.py file

---
+++ app.py
@@ -0,0 +1,12 @@
+# The main Flask application file
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Hello Flask"
+    
+if __name__ == "__main__":
+    app.run()
