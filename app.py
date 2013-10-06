702fdad32f2874a96ab59054cc498453330fff7e
ampersandss <duncan.mackey@gmail.com>
2013-10-05 21:06:13 -0700

    Adding basic templating foundations and temporary page design.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # The main Flask application file
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask import render_template
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + 'app.db'
@@ -9,6 +10,10 @@ db = SQLAlchemy(app)
 @app.route("/")
 def main():
     return "Hello Flask"
+	
+@app.route("schedule")
+def schedule():
+	return render_template('schedule.html')
     
 if __name__ == "__main__":
     app.run()
