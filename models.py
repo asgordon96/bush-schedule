8e68fbe10d3bef243e083d7c2f45c47568da949b
Aaron Gordon <asgordon96@gmail.com>
2013-10-05 17:31:05 -0700

    Intialize users and store password using bcrypt.

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 # The Main Database Models
+import bcrypt
 from app import db
 
 class User(db.Model):
@@ -6,3 +7,19 @@ class User(db.Model):
     bush_email = db.Column(db.String(100), index=True, unique=True)
     grade = db.Column(db.Integer)
     password_hash = db.Column(db.String(256))
+    
+    @classmethod
+    def authenticate(cls, email, password):
+        user = User.query.filter_by(bush_email=email).first()
+        if user:
+            if bcrypt.hashpw(password, user.password_hash) == user.password_hash:
+                return user
+        return False
+    
+    def __init__(self, email, password, grade=None):
+        self.bush_email = email
+        self.password_hash = bcrypt.hashpw(password, bcrypt.gensalt())
+        if grade:
+            self.grade = grade
+        
+    
