9a3ba9ecee78b3184fce2581d49a0e6b374b9748
Aaron Gordon <asgordon96@gmail.com>
2013-10-18 19:54:20 -0700

    Add database model for a class with a Block and a Name that belongs to a user

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     bush_email = db.Column(db.String(100), index=True, unique=True)
     grade = db.Column(db.Integer)
     password_hash = db.Column(db.String(256))
+    classes = db.relationship('Class', backref='user')
     
     @classmethod
     def authenticate(cls, email, password):
@@ -22,6 +23,21 @@ class User(db.Model):
         if grade:
             self.grade = grade
     
+    def __repr__(self):
+        return "%d %s" % (self.id, self.bush_email)
+    
     def change_password(self, new_password):
         self.password_hash = bcrypt.hashpw(new_password, bcrypt.gensalt())
+
+class Class(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    block = db.Column(db.String(1))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     
+    def __init__(self, block, name):
+        self.block = block
+        self.name = name
+    
+    def __repr__(self):
+        return "%s %s" % (self.block, self.name)
