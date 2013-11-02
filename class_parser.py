5b315426d09735d5a23f10060607f0e2d82fdd6c
Aaron Gordon <asgordon96@gmail.com>
2013-11-01 17:09:37 -0700

    Changed class_parser.py for winter and spring. Now used with command line args

--- class_parser.py
+++ class_parser.py
@@ -1,5 +1,7 @@
-def get_classes():
-    f = open("fall_2013.txt")
+import re
+
+def get_classes(filename, rows=45):
+    f = open(filename)
     data = f.read()
     f.close()
     data = data.split('\n')
@@ -10,8 +12,8 @@ def get_classes():
     teachers = []
     rooms = []
 
-    rows = 44
-
+    rows = rows
+    
     for i in range(8):
         d = data[i*rows:i*rows+rows]
         if i % 4 == 0:
@@ -23,7 +25,7 @@ def get_classes():
         else:
             rooms += d
 
-    end = data[7*rows+rows:-2]
+    end = data[7*rows+rows:]
     n = len(end) / 4
     for i in range(4):
         d = end[i*n:i*n+n]
@@ -35,7 +37,7 @@ def get_classes():
             teachers += d
         else:
             rooms += d
-    
+
     return zip(blocks, classes, teachers, rooms)
     
 def classes_by_block():
@@ -43,7 +45,7 @@ def classes_by_block():
     classes = {}
     for item in data:
         block = item[0].split()[0]
-        name = item[1].split("(F)")[0].split("(1)")[0].strip()
+        name = item[1].split("(F)")[0].split("(1)")[0].split("(W)")[0].strip()
         if block in classes.keys():
             classes[block].append(name)
         else:
@@ -52,13 +54,14 @@ def classes_by_block():
         classes[k].sort()
     return classes
 
-def save_as_csv(filename):
-    data = get_classes()
+def save_as_csv(infile, filename, rows):
+    data = get_classes(infile, rows)
     write = []
     for item in data:
         block = item[0].split()[0]
         subject = item[0].split()[1]
-        class_name = item[1].split("(F)")[0].split("(1)")[0].strip()
+        pattern = '\(W\)|\(1\)|\(2\)|\(W\3\)|\(W\)|\(S\)'
+        class_name = re.split(pattern, item[1])[0].strip()
         teacher = item[2]
         room = item[3]
         write.append(",".join([block, subject, class_name, teacher, room]))
@@ -66,3 +69,14 @@ def save_as_csv(filename):
     f = open(filename, "w")
     f.write(write)
     f.close()
+
+if __name__ == "__main__":
+    import sys
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+    if len(sys.argv) == 4:
+        rows = int(sys.argv[3])
+    else:
+        rows = 44
+    save_as_csv(infile, outfile, rows)
+    print "Saved to %s" % (outfile)
